81c80011402fd8321593476d7caac04d150adf61
DWraith23 <bwilson5924@gmail.com>
2025-06-07 23:26:26 -0400

    Add TweenToAnimationStart method for smooth animation transitions

--- Scripts/Tools.cs
+++ Scripts/Tools.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Threading.Tasks;
+using Godot;
+using Godot.Collections;
+using Microsoft.VisualBasic;
 
 namespace dudebattler.Scripts;
 
@@ -13,4 +17,45 @@ public static class Tools
     /// This can be used throughout the game to ensure consistent random number generation.
     /// </summary>
     public static Random Rand => new();
+
+
+    public static async Task TweenToAnimationStart(Node node, AnimationPlayer animator, string animationName, float duration)
+    {
+        if (animator.HasAnimation(animationName))
+        {
+            var animation = animator.GetAnimation(animationName);
+            var count = animation.GetTrackCount();
+
+            var tween = node.GetTree().CreateTween().SetParallel(true);
+
+            for (int i = 0; i < count; i++)
+            {
+                var path = animation.TrackGetPath(i).ToString();
+                var nodePath = path[..path.IndexOf(':')];
+                var tweenNode = node.GetNode(nodePath);
+                var property = path[(path.IndexOf(':') + 1)..];
+                Variant value;
+                var type = animation.TrackGetType(i);
+                if (type == Animation.TrackType.Value)
+                {
+                    value = animation.TrackGetKeyValue(i, 0);
+                }
+                else if (type == Animation.TrackType.Bezier)
+                {
+                    value = animation.BezierTrackGetKeyValue(i, 0);
+                }
+                else
+                {
+                    GD.PrintErr($"Unsupported track type: {type}");
+                    continue;
+                }
+                tween.TweenProperty(tweenNode, property, value, duration);
+            }
+            await tween.Completed();
+        }
+        else
+        {
+            GD.PrintErr($"Animation '{animationName}' not found in AnimationPlayer.");
+        }
+    }
 }
