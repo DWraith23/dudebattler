ae672c969cdbfb90752e9358982835d76ab446e1
DWraith23 <bwilson5924@gmail.com>
2025-07-02 19:14:47 -0400

    Fix namespace casing and remove unused using directives in ValueResource class

--- Resources/ValueResource.cs
+++ Resources/ValueResource.cs
@@ -1,11 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using dudebattler.Scripts;
+using DudeBattler.Scripts;
 using Godot;
 
-namespace dudebattler.Resources;
+namespace DudeBattler.Resources;
 
 public partial class ValueResource : Resource
 {
